89d841ef8847d6de69c72a008194283bea43f09a
EdgarRepositorio <edgargonzalez.info@gmail.com>
2017-02-16 18:16:26 +0100

    he establecido en la clase BusquedaPane que  el  campo de busqueda sea obligatorio y te mustra un JOPtionPane

--- src/main/java/com/leonardo/gym/view/BusquedaPane.java
+++ src/main/java/com/leonardo/gym/view/BusquedaPane.java
@@ -189,7 +189,7 @@ public class BusquedaPane extends javax.swing.JDialog {
 
     private void btnBusquedaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBusquedaActionPerformed
        
-        
+        //cometario
         if (txtBusqueda.getText().equals("")) {
             JOptionPane.showMessageDialog(panCliente, "Campo Obligatorio");
 
